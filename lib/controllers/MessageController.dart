3d831958ab8003a5ebee573792591fbab94e5425
Aimée_Amisa <amisangandu2018@gmail.com>
2023-06-08 11:03:06 +0100

    liste des conversation et messages

--- lib/controllers/MessageController.dart
+++ lib/controllers/MessageController.dart
@@ -1,15 +1,11 @@
-import 'dart:convert';
-import 'dart:ffi';
 import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:path/path.dart';
 import 'package:squelette_mobile_parcours/models/MessageModel.dart';
 import 'package:squelette_mobile_parcours/models/ConversationModel.dart';
 import '../utils/Constantes.dart';
 import '../utils/Endpoints.dart';
 import '../utils/Request.dart';
 import '../utils/StockageKeys.dart';
-import 'package:http/http.dart' as http;
 
 class MessageController with ChangeNotifier {
   GetStorage? stockage;
@@ -19,7 +15,7 @@ class MessageController with ChangeNotifier {
 
   MessageController({this.stockage});
 
-  void EnvoyerMessage(Map data) async{
+  void envoyerMessage(Map data) async{
     var url = Endpoints.EnvoyerMessage;
     var tkn = stockage?.read(StockageKeys.tokenyKey);
     var reponse = await postData(url, data,token:tkn );
@@ -31,33 +27,9 @@ class MessageController with ChangeNotifier {
     var reponse = await postData(url, data,token:tkn );
     print("Resultat ${reponse.data}");
   }
-  void RecevoirListeConversation() async{
+  void recevoirListeConversation() async{
 
-    var url ="${Endpoints.ListeConversation}";
-    loading = true;
-    notifyListeners();
-
-    var reponse = await getData(url);
-    print("Résultat de la réccupération ${reponse}");
-
-    if(reponse!=null){
-      messages=reponse["data"].map<ConversationModel>((e) => ConversationModel.fromJson(e)).toList();
-      stockage?.write(StockageKeys.ListeConversationKey, reponse);
-      isHttpException=false;
-      notifyListeners();
-    }else{
-      isHttpException=true;
-      var datastockee= stockage?.read(StockageKeys.ListeConversationKey);
-      var temp= datastockee["data"].map<MessageModel>((e)=>ConversationModel.fromJson(e)).toList;
-      messages = temp;
-      print("data stocké :${temp}");
-    }
-    loading = false;
-    notifyListeners();
-
-  }
-  void RecevoirListeMessage() async{
-    var url ="${Endpoints.ListeMessage}";
+    var url ="${Endpoints.CreerConversation}";
     loading = true;
     notifyListeners();
 
@@ -65,14 +37,38 @@ class MessageController with ChangeNotifier {
     print("Résultat de la réccupération ${reponse}");
 
     if(reponse!=null){
-      messages=reponse["data"].map<MessageModel>((e) => MessageModel.fromJson(e)).toList();
-      stockage?.write(StockageKeys.RecevoirListeMessagekey, reponse);
+      messages=reponse.map<MessageModel>((e) => ConversationModel.fromJson(e)).toList();
+      stockage?.write(StockageKeys.tokenyKey, reponse);
       isHttpException=false;
       notifyListeners();
     }else{
       isHttpException=true;
-      var datastockee= stockage?.read(StockageKeys.RecevoirListeMessagekey);
-      var temp= datastockee["data"].map<MessageModel>((e)=>MessageModel.fromJson(e)).toList;
+      var datastockee= stockage?.read(StockageKeys.tokenyKey);
+      var temp= datastockee.map<MessageModel>((e)=>ConversationModel.fromJson(e)).toList;
+      messages = temp;
+      print("data stocké :${temp}");
+    }
+    loading = false;
+    notifyListeners();
+
+  }
+  void recevoirListeMessage() async{
+    var url ="${Endpoints.EnvoyerMessage}";
+    loading = true;
+    notifyListeners();
+
+    var reponse = await getData(url, token: Constantes.DefaultToken);
+    print("Résultat de la réccupération ${reponse}");
+
+    if(reponse!=null){
+      messages=reponse.map<MessageModel>((e) => MessageModel.fromJson(e)).toList();
+      stockage?.write(StockageKeys.tokenyKey, reponse);
+      isHttpException=false;
+      notifyListeners();
+    }else{
+      isHttpException=true;
+      var datastockee= stockage?.read(StockageKeys.tokenyKey);
+      var temp= datastockee.map<MessageModel>((e)=>MessageModel.fromJson(e)).toList;
       messages = temp;
       print("data stocké :${temp}");
     }
@@ -80,10 +76,11 @@ class MessageController with ChangeNotifier {
     notifyListeners();
   }
 }
-
 void main() {
   var test = MessageController();
   //test.RecevoirListeMessage();
   //test.creerConversationApi({"article_id":2,"user_id":1});
-  test.EnvoyerMessage({"conversation_id":1,"content":"bonsoirtrrrr"});
+  //test.EnvoyerMessage({"conversation_id":1,"content":"bonsoirtrrrr"});
+  //test.recevoirListeMessage();
+  test.recevoirListeConversation();
 }
